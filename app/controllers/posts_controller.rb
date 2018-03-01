2e61d9f61d8f8e531b11d80aad4f02ab45ad05c7
roncode <ron_benporat@yahoo.com>
2018-03-01 23:45:07 +0000

    update nav to change when logged in vs. not, add user_id to posts, add associations to post and user models

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
@@ -10,7 +11,9 @@ class PostsController < ApplicationController
   end
   
   def create
-    if @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+    
+    if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to posts_path
     else
