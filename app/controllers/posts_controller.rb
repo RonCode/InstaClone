eced903146cc13f8602ad6563c1800aee657f951
roncode <ron_benporat@yahoo.com>
2018-03-01 18:54:20 +0000

    DRY posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
     @posts = Post.all
@@ -9,27 +10,32 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.create(post_params)
-    
-    redirect_to root_path
+    if @post = Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
   
   def show
-    @post = Post.find(params[:id])
+    #set_post before action
   end
   
   def edit
-    @post = Post.find(params[:id])
+    #set_post before action
   end
   
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    #set_post before action
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
   
   def destroy
-    @post = Post.find(params[:id])
+    #set_post before action
     @post.destroy
     redirect_to root_path
   end
@@ -37,6 +43,10 @@ class PostsController < ApplicationController
   private
     def post_params
       params.require(:post).permit(:image, :caption)
+    end
+    
+    def set_post
+      @post = Post.find(params[:id])
     end
     
 end
