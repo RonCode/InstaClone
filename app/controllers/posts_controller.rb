fe5fc2809608bcda2fe2dc56ca6c2bafc9fe5256
roncode <ron_benporat@yahoo.com>
2018-03-01 18:44:43 +0000

    add delete action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,6 +28,12 @@ class PostsController < ApplicationController
     redirect_to(post_path(@post))
   end
   
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+  
   private
     def post_params
       params.require(:post).permit(:image, :caption)
