3d9ae5235a430a1c3e6a80264aab782e50933b06
roncode <ron_benporat@yahoo.com>
2018-03-02 00:17:18 +0000

    update view if post is owned by user and update control to protect personal posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :owned_post, only: [:edit, :update, :destroy]
   
   def index
     @posts = Post.all
@@ -56,4 +57,10 @@ class PostsController < ApplicationController
       @post = Post.find(params[:id])
     end
     
+    def owned_post
+      unless current_user == @post.user
+        flash[:alert] = "That post doesn't belong to you!"
+        redirect_to root_path
+      end
+    end
 end
